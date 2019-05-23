0f24a1bab3fa4c195cc561440bc3c869d722b50c
Valentyn Yakymenko <vyakimenko@codeminders.com>
2019-05-23 13:07:07 +0300

    * wip

--- server/services/name-list.static.spec.ts
+++ server/services/name-list.static.spec.ts
@@ -1,5 +1,6 @@
 import { app } from '../app';
 import * as chai from 'chai';
+// @ts-ignore
 import chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
@@ -8,7 +9,7 @@ describe('GET /api/name-list/static', () => {
   it('should return response on call', () => {
     return chai.request(app).get('/api/name-list/static')
       .then(res => {
-        chai.expect(res.text).to.eql([]);
+        chai.expect(res.text).to.eql(`["Edsger Dijkstra","Donald Knuth","Alan Turing","Grace Hopper"]`);
       });
   });
 });
