66f0062c4f457b805ef5681c9bb7571ba95684ab
Valentyn Yakymenko <vyakimenko@codeminders.com>
2019-05-23 13:27:20 +0300

    * unit test update

--- server/services/name-list.static.spec.ts
+++ server/services/name-list.static.spec.ts
@@ -6,7 +6,7 @@ import chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 describe('GET /api/name-list/static', () => {
-  it('should return response on call', () => {
+  it('should return response', () => {
     return chai.request(app).get('/api/name-list/static')
       .then(res => {
         chai.expect(res.text).to.eql(`["Edsger Dijkstra","Donald Knuth","Alan Turing","Grace Hopper"]`);
